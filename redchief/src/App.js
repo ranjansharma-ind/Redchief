9c8e2654cce182aaa923d2e15781427f9d76dd03
Ranjan Sharma <ranjansharma.ind@gmail.com>
2024-04-24 12:19:41 +0530

    commit

--- redchief/src/App.js
+++ redchief/src/App.js
@@ -9,29 +9,32 @@ function App() {
       <header className="App-header">
         There are two links in this QR Code
       </header>
-      <div className='container'>
-        <Container>
-          <Row>
-            <Col sm={6}>
-              <Card className='card'>
-                <CardBody>
-                  <a href='https://redchief.in/furo-sports'><img src={image1} height={200} /></a>
-                </CardBody>
-                <CardText>Red Rechief</CardText>
-              </Card>
-            </Col>
-            <Col sm={6}>
-              <Card className='card'>
-                <CardBody>
-                  <a href='https://redchief.in/furo-sports'><img src={image2} height={200} /></a>
-                </CardBody>
-                <CardText>Futo Sports</CardText>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+      <div className='Main'>
+        <div className='container'>
+          <Container>
+            <Row>
+              <Col sm={6}>
+                <Card className='card'>
+                  <CardBody>
+                    <a href='https://redchief.in/furo-sports'><img src={image1} height={200} /></a>
+                  </CardBody>
+                  <CardText>Red Rechief</CardText>
+                </Card>
+              </Col>
+              <Col sm={6}>
+                <Card className='card'>
+                  <CardBody>
+                    <a href='https://redchief.in/furo-sports'><img src={image2} height={200} /></a>
+                  </CardBody>
+                  <CardText>Futo Sports</CardText>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     </div>
+
   );
 }
 
